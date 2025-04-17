f39c12a28fbfff81802137ae83842efbee30f7a8
Alex Miller <alexmiller422@gmail.com>
2025-04-17 11:11:15 +0100

    Make a content collection

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,13 +1,20 @@
-import rss, { pagesGlobToRssItems } from '@astrojs/rss';
+import rss from '@astrojs/rss';
+import { getCollection } from "astro:content";
 
 const base = import.meta.env.BASE_URL
 
 export async function GET(context) {
+    const posts = await getCollection("blog");
     return rss({
         title: 'Astro Learner | Blog',
         description: 'My journey learning Astro',
         site: `${context.site}/${base}`,
-        items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
+        items: posts.map((post => ({
+            title: post.data.title,
+            pubDate: post.data.pubDate,
+            description: post.data.description,
+            link: `${base}/posts/${post.id}`
+        }))),
         customData: `<language>en-us</language>`,
     });
 }
